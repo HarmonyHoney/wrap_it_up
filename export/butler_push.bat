e4b2a391655e8f0e1c35bc75df54e30c62110044
Harmony Honey <HarmonyMonroe97@gmail.com>
2023-06-05 23:08:15 -0400

    desktop exports (=

--- export/butler_push.bat
+++ export/butler_push.bat
@@ -1,2 +1,5 @@
 butler push web harmonyhoney/wrap-it-up:web
+butler push win harmonyhoney/wrap-it-up:win
+butler push linux harmonyhoney/wrap-it-up:linux
+butler push mac harmonyhoney/wrap-it-up:mac
 pause
--- export/ready_folders.bat
+++ export/ready_folders.bat
@@ -1,2 +1,11 @@
 rmdir /s /q web
 mkdir web
+
+rmdir /s /q win
+mkdir win
+
+rmdir /s /q linux
+mkdir linux
+
+rmdir /s /q mac
+mkdir mac
